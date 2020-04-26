86409d8eaa306d029b47968ce057a9463f9ae257
Niscolinx <munisco12@gmail.com>
2020-04-26 10:13:03 +0100

    updated home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,7 +10,12 @@ class Home extends Component{
 
     render(){
         return(
-
+            <div>
+                <h1>This is the home</h1>
+            </div>
         )
     }
 }
+
+
+export default Home
