e71c4037f6836331bcc990f5844855ab52c30701
Niscolinx <munisco12@gmail.com>
2020-04-26 10:58:19 +0100

    Added class to the home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -12,7 +12,7 @@ class Home extends Component{
     render(){
         return(
             <div>
-                <h1>This is the home</h1>
+                <h1 className = 'home'>This is the home</h1>
                 <Button/>
             </div>
         )
