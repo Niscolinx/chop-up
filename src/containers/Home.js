61a3d21e5446f34368a73b006a261d84765a16b0
Niscolinx <munisco12@gmail.com>
2020-04-30 22:31:24 +0100

    updated Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -12,7 +12,7 @@ class Home extends Component{
 
     render(){
         return(
-            <div>
+            <div className = 'Home'>
 
                 <Container fliuid>
                     <Row>
