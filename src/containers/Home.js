b4a9aa29903325c95157bf49daf60b8c28a4e312
Niscolinx <munisco12@gmail.com>
2020-04-30 22:38:58 +0100

    updated Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,7 +15,7 @@ class Home extends Component{
             <div className = 'home'>
                 hellg
 
-                <Container fliuid className = 'home'>
+                <Container fliuid>
                     <Row>
                 
                         <Col><h1>Everything you need, <span>delivered within minutes</span></h1></Col>
