99392bb63e51b42684407085f6cabfbe3ebdac6a
Niscolinx <munisco12@gmail.com>
2020-05-03 00:59:50 +0100

    connected to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,8 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: 'AIzaSyCkxoSmDMAvGH5Uyd2XQuk6ghxHOTjhSB4',
-    authDomain: 'my-react-burger-1ce01.firebaseapp.com',
-    databaseURL: 'https://my-react-burger-1ce01.firebaseio.com',
-    projectId: 'my-react-burger-1ce01',
-    storageBucket: 'my-react-burger-1ce01.appspot.com',
-    messagingSenderId: '1090815861850',
-    appId: '1:1090815861850:web:4fe6264163a25f3ae0e463'
 
-     apiKey: "AIzaSyC0ftZT-5WylSWql7hDGuBYaEfkZPBWthQ",
+    apiKey: "AIzaSyC0ftZT-5WylSWql7hDGuBYaEfkZPBWthQ",
     authDomain: "chopup-2f24c.firebaseapp.com",
     databaseURL: "https://chopup-2f24c.firebaseio.com",
     projectId: "chopup-2f24c",
