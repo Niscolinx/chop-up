76a6d614e3436515541b553284a13bd6415d1cb6
Niscolinx <munisco12@gmail.com>
2020-05-02 23:09:59 +0100

    created variables

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,56 +1,58 @@
-import React, {Component} from 'react'
-import { Button, Container, Row, Col} from 'react-bootstrap';
+import React, { Component } from 'react'
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form'
 import imgUrl from '../assets/images/background_home.jpg'
 
 
 
-class Home extends Component{
+class Home extends Component {
 
-    state ={
+    state = {
 
     }
 
-    render(){
+    render() {
         const divStyle = {
             backgroundImage: 'url(' + imgUrl + ')',
             width: '100%',
             height: '95vh',
             backgroundSize: 'cover',
         };
-        return(
-            <div style = {divStyle}>
-                <Container fliuid className = 'hero'>
+        return (
+            <div style={divStyle}>
+                <Container fliuid className='hero'>
                     <Row>
-                
-                        <Col><h1 className = 'hero_heading'>Everything you need, <span className='hero_heading--span'>delivered within minutes</span></h1></Col>
-                
+
+                        <Col><h1 className='hero_heading'>Everything you need,</h1>
+                            <h1 className='hero_heading--span'>delivered within minutes</h1>
+                        </Col>
+
                         <>
                         </>
                     </Row>
                             delivering to
-                            <Form className = 'delivering-form'>
-                                <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-                                    <Form.Label>Custom select Small</Form.Label>
-                                    <Form.Control as="select" size="sm" custom>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </Form.Control>
-                                </Form.Group>
-                                <Form.Group controlId="exampleForm.SelectCustomSizeLg">
-                                    <Form.Label>Custom select Large</Form.Label>
-                                    <Form.Control as="select" size="lg" custom>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </Form.Control>
-                                </Form.Group>
-                            </Form>
+                            <Form className='delivering-form'>
+                        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
+                            <Form.Label>Custom select Small</Form.Label>
+                            <Form.Control as="select" size="sm" custom>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.SelectCustomSizeLg">
+                            <Form.Label>Custom select Large</Form.Label>
+                            <Form.Control as="select" size="lg" custom>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
                 </Container>
             </div>
         )
