875d337e28d22f7e54fe50296b7aa4dde56b6e47
Niscolinx <munisco12@gmail.com>
2020-05-03 00:59:15 +0100

    connected to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,6 +8,15 @@ var firebaseConfig = {
     storageBucket: 'my-react-burger-1ce01.appspot.com',
     messagingSenderId: '1090815861850',
     appId: '1:1090815861850:web:4fe6264163a25f3ae0e463'
+
+     apiKey: "AIzaSyC0ftZT-5WylSWql7hDGuBYaEfkZPBWthQ",
+    authDomain: "chopup-2f24c.firebaseapp.com",
+    databaseURL: "https://chopup-2f24c.firebaseio.com",
+    projectId: "chopup-2f24c",
+    storageBucket: "chopup-2f24c.appspot.com",
+    messagingSenderId: "750911912844",
+    appId: "1:750911912844:web:db94fc54d5de3ee0aab4a9",
+    measurementId: "G-JEHLV22Y6T"
 }
 
 const fire = firebase.initializeApp(firebaseConfig)
