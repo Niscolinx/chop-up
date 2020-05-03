7d54f2da4d2f1b635fcfb1d578449883d7e27be7
Niscolinx <munisco12@gmail.com>
2020-05-03 08:58:40 +0100

    styling the hero texts

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -35,7 +35,7 @@ class Home extends Component {
                             <Form className='hero_form'>
                         <Form.Group>
                             <Form.Label>Custom select Small</Form.Label>
-                            <Form.Control as="select" size="lg" custom width='100px'>
+                            <Form.Control as="select" size="lg" custom className='hero_form--control'>
                                 <option>1</option>
                                 <option>2</option>
                                 <option>3</option>
