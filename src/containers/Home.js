ca898bf340cf526a7fdb7fa4debd436fcab47d15
Niscolinx <munisco12@gmail.com>
2020-05-04 11:17:41 +0100

    Added placeholder to the select forms

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -45,7 +45,7 @@ class Home extends Component {
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Custom select Large</Form.Label>
-                            <Form.Control as="select" size="lg" custom className='hero_form--control' value="choose area">
+                            <Form.Control as="select" size="lg" custom className='hero_form--control'>
                                 <option>1</option>
                                 <option>2</option>
                                 <option>3</option>
