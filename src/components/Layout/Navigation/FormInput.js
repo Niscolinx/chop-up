7a7ccb8a66d24d40ca5ee476b7237c7a5d34c256
Niscolinx <munisco12@gmail.com>
2020-05-05 07:09:33 +0100

    put the form items into a function

--- src/components/Layout/Navigation/FormInput.js
+++ src/components/Layout/Navigation/FormInput.js
@@ -4,12 +4,13 @@ let cities = ['okigwe', 'uture', 'Absu', 'Enugu']
 let areas = ['Upgate', 'Owerri-road', 'student affairs', 'School cafe']
 
 const formInput = (props) => {
-    return {
-        this.props.map(item => {
-            let lower = item.toLowerCase()
-            let upper = item.charAt(0).toUpperCase() + item.slice(1)
-            return <option value={lower}>{upper}</option>
+    this.props.map(item => {
+        let lower = item.toLowerCase()
+        let upper = item.charAt(0).toUpperCase() + item.slice(1)
+        return <option value={lower}>{upper}</option>
 
-        }
-})
+
+    })
 }
+
+export default formInput
