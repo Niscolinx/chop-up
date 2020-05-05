99b490ff087cbca876fab66c4bccef596e0cd250
Niscolinx <munisco12@gmail.com>
2020-05-05 07:09:50 +0100

    put the form items into a function

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 import imgUrl from '../assets/images/background_home.jpg'
 
