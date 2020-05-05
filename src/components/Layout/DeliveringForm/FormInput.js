4a49a3abd1534b968ddbd1e82bb575d5315f5c3f
Niscolinx <munisco12@gmail.com>
2020-05-05 08:10:18 +0100

    Added the loctions list

--- src/components/Layout/DeliveringForm/FormInput.js
+++ src/components/Layout/DeliveringForm/FormInput.js
@@ -3,12 +3,14 @@ import React from 'react'
 const formInput = (props) => {
     console.log('this is the city',props.city)
     console.log('this is the area', props.area)
-    props.map(item => {
+   let location =  props.area.map(item => {
         let lower = item.toLowerCase()
         let upper = item.charAt(0).toUpperCase() + item.slice(1)
         return <option value={lower}>{upper}</option>
 
     })
+
+    return location
 }
 
 export default formInput
