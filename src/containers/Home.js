2b62dd5ec9b9e9aede40725a2177c05107cafe40
Niscolinx <munisco12@gmail.com>
2020-05-05 07:21:53 +0100

    Added the loctions list

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 import imgUrl from '../assets/images/background_home.jpg'
-import FormInput from '../components/Layout/FormInput'
+import FormInput from '../components/Layout/DeliveringForm/FormInput'
 
 
 class Home extends Component {
