509353c9c3fdf13050a16689637ec7253e342d91
Niscolinx <munisco12@gmail.com>
2020-05-06 23:21:53 +0100

    Did not work well with react-select-search

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,90 +1,90 @@
-// import React, { Component } from 'react'
-// import { Container, Row, Col } from 'react-bootstrap'
-// import Form from 'react-bootstrap/Form'
-// import imgUrl from '../assets/images/background_home.jpg'
-// import * as locations from '../components/Layout/DeliveringForm/Locations'
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
+import imgUrl from '../assets/images/background_home.jpg'
+import * as locations from '../components/Layout/DeliveringForm/Locations'
 
 
-// class Home extends Component {
+class Home extends Component {
 
-//     state = {
+    state = {
 
-//     }
+    }
 
-//     render() {
-//         const divStyle = {
-//             backgroundImage: 'url(' + imgUrl + ')',
-//             width: '100%',
-//             height: '95vh',
-//             backgroundSize: 'cover',
-//         };
-
-
+    render() {
+        const divStyle = {
+            backgroundImage: 'url(' + imgUrl + ')',
+            width: '100%',
+            height: '95vh',
+            backgroundSize: 'cover',
+        };
 
 
-//         const selectCity = locations.cities.map((item, i) => {
-//             let lower = item.toLowerCase()
-//             let upper = item.charAt(0).toUpperCase() + item.slice(1)
-//             return <option value={lower} key={item + i} className='delivering_form--locations_item'>{upper}</option>
 
-//         })
-//         const selectArea = locations.areas.map((item, i) => {
-//             let lower = item.toLowerCase()
-//             let upper = item.charAt(0).toUpperCase() + item.slice(1)
+
+        const selectCity = locations.cities.map((item, i) => {
+            let lower = item.toLowerCase()
+            let upper = item.charAt(0).toUpperCase() + item.slice(1)
+            return <option value={lower} key={item + i} className='delivering_form--locations_item'>{upper}</option>
+
+        })
+        const selectArea = locations.areas.map((item, i) => {
+            let lower = item.toLowerCase()
+            let upper = item.charAt(0).toUpperCase() + item.slice(1)
             
-//             return <option value={lower} key={item + i} className='delivering_form--locations_item'>{upper}</option>
+            return <option value={lower} key={item + i} className='delivering_form--locations_item'>{upper}</option>
 
-//         })
+        })
 
 
-//         return (
-//             <div style={divStyle}>
-//                 <Container fliuid className='hero'>
-//                     <Row>
+        return (
+            <div style={divStyle}>
+                <Container fliuid className='hero'>
+                    <Row>
 
-//                         <Col className='hero_heading'>
-//                             <h1 className='hero_heading--main'>Everything you need,</h1>
-//                             <h1 className='hero_heading--span'>delivered within minutes</h1>
-//                         </Col>
+                        <Col className='hero_heading'>
+                            <h1 className='hero_heading--main'>Everything you need,</h1>
+                            <h1 className='hero_heading--span'>delivered within minutes</h1>
+                        </Col>
 
-//                     </Row>
+                    </Row>
 
-//                     <Row className='delivering'>
-//                         <h3 className='delivering_text'>Delivering to</h3>
-//                         <Form className='delivering_form'>
-//                             <Form.Group>
-//                                 <Form.Control as="select" size="lg" custom className='delivering_form--control'>
-//                                     <option value="" disabled selected hidden className='delivering_form--control-placeholder'>Choose your city</option>
+                    <Row className='delivering'>
+                        <h3 className='delivering_text'>Delivering to</h3>
+                        <Form className='delivering_form'>
+                            <Form.Group>
+                                <Form.Control as="select" size="lg" custom className='delivering_form--control'>
+                                    <option value="" disabled selected hidden className='delivering_form--control-placeholder'>Choose your city</option>
 
-//                                     {selectCity}
-//                                 </Form.Control>
-//                             </Form.Group>
-//                             <Form.Group>
-//                                 <Form.Control as="select" size="lg" custom className='delivering_form--control' title="Choose one of the following...">
-//                                     {/* <option value="" disabled selected hidden className='delivering_form--control-placeholder'>Select your area</option> */}
+                                    {selectCity}
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Control as="select" size="lg" custom className='delivering_form--control' title="Choose one of the following...">
+                                    {/* <option value="" disabled selected hidden className='delivering_form--control-placeholder'>Select your area</option> */}
 
-//                                     {selectArea}
-//                                 </Form.Control>
+                                    {selectArea}
+                                </Form.Control>
 
                                 
 
-//                             </Form.Group>
+                            </Form.Group>
 
 
-//                         </Form>
-//                     </Row>
+                        </Form>
+                    </Row>
 
-//                     {/* <>
-//                     <h1>Restaurant</h1>
-//                     <h1>Restaurant</h1>
-//                     <h1>Groceries</h1>
-//                     <h1>Restaurants</h1> 
-//                     </>*/}
-//                 </Container>
-//             </div>
-//         )
-//     }
-// }
+                    {/* <>
+                    <h1>Restaurant</h1>
+                    <h1>Restaurant</h1>
+                    <h1>Groceries</h1>
+                    <h1>Restaurants</h1> 
+                    </>*/}
+                </Container>
+            </div>
+        )
+    }
+}
 
 
-// export default Home
+export default Home
