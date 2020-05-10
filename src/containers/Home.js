66c3ca016d0bfab77782b22105722e2869593376
Niscolinx <munisco12@gmail.com>
2020-05-10 11:00:44 +0100

    Added data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -59,9 +59,7 @@ class Home extends Component {
 
                 <Container>
                     <Row className="justify-content-md-center">
-                        <Col xs lg="2">
-                            1 of 3
-    </Col>
+                       
                         <Col md="auto">                            <h1 className='hero_heading--main'>Everything you need,</h1>
                             <h1 className='hero_heading--span'>delivered within minutes</h1></Col>
                         <Col xs lg="2">
