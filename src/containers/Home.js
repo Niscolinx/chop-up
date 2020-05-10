2719bd8c742488f9a127e9d9434570157f95ebb5
Niscolinx <munisco12@gmail.com>
2020-05-10 10:38:30 +0100

    Added data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -56,7 +56,7 @@ class Home extends Component {
 
         return (
             <div style={divStyle}>
-                <Container fliuid className='hero'>
+                <Container fluid>
                     <Row>
 
                         <Col className='hero_heading'>
