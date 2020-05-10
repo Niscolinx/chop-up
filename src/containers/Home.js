73aa2a5c04e7c621586f8787e59f6d167b653d8c
Niscolinx <munisco12@gmail.com>
2020-05-10 21:52:47 +0100

    added the secondary color

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -205,6 +205,20 @@ class Home extends Component {
                                 <p className="newsletter_description">Don't miss out on your great offers and Receive deals from out top vendors via e-mail</p>
                             </Col>
                         </Row>
+
+                        <Form>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                            </Form.Row>
+                            </Form>
                     </Container>
                 </div>
             </div>
