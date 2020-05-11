2a616afe7c50b9da04821b4af1d55d56fb7228f0
Niscolinx <munisco12@gmail.com>
2020-05-11 14:56:51 +0100

    Styling the card section

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -279,6 +279,46 @@ class Home extends Component {
 
 
                         </CardGroup>
+                        <CardGroup>
+                            <Card>
+                                <Card.Img variant="top" src={require('../assets/images/PoundedYam.jpg')} />
+                                <Card.Body>
+                                    <Card.Title>Smart Bite - Upgate</Card.Title>
+                                    <Card.Text>
+                                        Min Order N0. Delivering fee N200
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+                                <Card.Img variant="top" src={require('../assets/images/PoundedYam.jpg')} />
+                                <Card.Body>
+                                    <Card.Title>Smart Bite - Upgate</Card.Title>
+                                    <Card.Text>
+                                        Min Order N0. Delivering fee N200
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+                                <Card.Img variant="top" src={require('../assets/images/PoundedYam.jpg')} />
+                                <Card.Body>
+                                    <Card.Title>Smart Bite - Upgate</Card.Title>
+                                    <Card.Text>
+                                        Min Order N0. Delivering fee N200
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                </Card.Footer>
+                            </Card>
+
+
+                        </CardGroup>
                     </Container>
 
                 </div>
