8001e740aee1f6fcb18ef6317f6c9b0bfef1dd47
Niscolinx <munisco12@gmail.com>
2020-05-12 02:51:14 +0100

    Styling the footer rows

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -342,16 +342,12 @@ class Home extends Component {
                 </div>
                 <footer class="page-footer font-small indigo">
 
-                    <!-- Footer Links -->
   <div class="container text-center text-md-left">
 
-                        <!-- Grid row -->
     <div class="row">
 
-                            <!-- Grid column -->
       <div class="col-md-3 mx-auto">
 
-                                <!-- Links -->
         <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
 
                                 <ul class="list-unstyled">
@@ -370,7 +366,6 @@ class Home extends Component {
                                 </ul>
 
                             </div>
-                            <!-- Grid column -->
 
       <hr class="clearfix w-100 d-md-none">
 
