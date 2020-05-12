7b13758e9ea68aa6c7ef8fdf8a0da36ce33e1682
Niscolinx <munisco12@gmail.com>
2020-05-12 02:22:35 +0100

    Added the footer rows

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -327,9 +327,9 @@ class Home extends Component {
                     <div className='footer_1'>
 
                         <div className='footer_1--Cities'></div>
-                        <div className='footer_1--Cities'></div>
+                        <div className='footer_1--Stores'></div>
                         <div className='footer_1--popular_vendors'>Popular Vendors</div>
-                        <div className='footer_1--chop_up_delivery'>Chop Up delivery</div>
+                        <div className='footer_1--chop_up_delivery'>ChopUp Delivery</div>
                     </div>
                     <div className='footer_2'>
 
