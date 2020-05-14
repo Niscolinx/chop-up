814f5717b838f797d136a2a6e30818c6d96a4083
Niscolinx <munisco12@gmail.com>
2020-05-14 22:08:46 +0100

    Added the cities

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -335,10 +335,10 @@ class Home extends Component {
 
                                 <ul className="list-unstyled">
                                     <li>
-                                        <a href="#!" className='footer_link'>Very long link 1</a>
+                                        <a href="#!" className='footer_link'>Absu</a>
                                     </li>
                                     <li>
-                                        <a href="#!" className='footer_link'>Very long link 1</a>
+                                        <a href="#!" className='footer_link'>Umuahia</a>
                                     </li>
                                     <li>
                                         <a href="#!" className='footer_link'>Very long link 1</a>
