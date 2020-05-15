aeeb6e72ee8c47534f57deec99762b44f6d4b76b
Niscolinx <munisco12@gmail.com>
2020-05-15 17:23:12 +0100

    Styling the react select and changed the height

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -46,7 +46,7 @@ class Home extends Component {
             return <option value={lower} key={item + i} className='delivering_form--locations_item'>{upper}</option>
 
         })
-        const customStyles = (height= 50) => {
+        const customStyles = (height= '3rem') => {
             return{
 
                 menu: (provided, state) => ({
