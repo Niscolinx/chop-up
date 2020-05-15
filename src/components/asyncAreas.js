c3ebc67441565f5249c4a8fe1a4962f4b5d55d82
Niscolinx <munisco12@gmail.com>
2020-05-15 18:58:55 +0100

    Addes asynchronous loading of the areas

--- src/components/asyncAreas.js
+++ src/components/asyncAreas.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
+
     render() {
+        let okigwe = []
 
         let ingredient = null;
 
