e20e16c9031f7fde3cb9328de68de33b3cfbbb1e
Niscolinx <munisco12@gmail.com>
2020-05-15 17:03:47 +0100

    Styling the react select

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -61,12 +61,31 @@ class Home extends Component {
                 cursor: 'pointer',
                 padding: '1rem'
             }),
-            control: (styles) => ({
-                ...styles,
+            control: (provided, state) => ({
+                ...provided,
                 cursor: 'pointer',
-                minHeight: '40px',
-                height: '40px'
+                background: '#fff',
+                borderColor: '#9e9e9e',
+                minHeight: '30px',
+                height: '30px',
+                boxShadow: state.isFocused ? null : null,
+            }),
+            valueContainer: (provided, state) => ({
+                ...provided,
+                height: '30px',
+                padding: '0 6px'
+            }),
 
+            input: (provided, state) => ({
+                ...provided,
+                margin: '0px',
+            }),
+            indicatorSeparator: state => ({
+                display: 'none',
+            }),
+            indicatorsContainer: (provided, state) => ({
+                ...provided,
+                height: '30px',
             }),
           
 
