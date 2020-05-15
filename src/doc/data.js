149638546315f68be841f95693894c8dc12f4a17
Niscolinx <munisco12@gmail.com>
2020-05-15 18:31:58 +0100

    Styling the react select

--- src/doc/data.js
+++ src/doc/data.js
@@ -1,5 +1,5 @@
 export const colourOptions = [
-    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'Absu', label: 'Absu',isFixed: true },
     { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
     { value: 'purple', label: 'Purple', color: '#5243AA' },
     { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
