d995580e8edeff6d277485867c1e01e7637a3455
Niscolinx <munisco12@gmail.com>
2020-05-18 11:08:16 +0100

    Adding functions to handle the city changes and load areas asynchronously

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -23,21 +23,14 @@ const addIngredient = (state, action) => {
             ...state.ingredients,
             [action.ingredientName]: state.ingredients[action.ingredientName] + 1
         },
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
     })
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-            }
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action)
+           
         case actionTypes.REMOVE_INGREDIENT:
             return {
                 ...state,
