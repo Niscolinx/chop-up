e7e393e0c789bb43a0c77e50ce8238d2446e67a5
Niscolinx <munisco12@gmail.com>
2020-05-21 12:48:35 +0100

    restructuring and styling

--- src/components/Layout/Hero.js
+++ src/components/Layout/Hero.js
@@ -248,9 +248,7 @@ import { cityOptions } from '../../doc/data';
                             <h1 class="hero_heading-primary">
                                 <span class="hero_heading-primary--main">Everything you need,</span> <span class="hero_heading-primary--sub">delivered within minutes</span>
                             </h1>
-                            <h1 class="hero_heading-primary">
-                               Everything you need,primary--sub">delivered within minutes
-                            </h1>
+                      
                             
                             
                         </Col>
@@ -258,7 +256,7 @@ import { cityOptions } from '../../doc/data';
                         
                          
                             <h1 class="hero_heading-primary">
-                               Everything you need,primary--sub">delivered within minutes
+                               Everything you need, delivered within minutes
                             </h1>
                             
                             
