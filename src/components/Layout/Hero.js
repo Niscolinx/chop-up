b0a0225f73856b234b8eda9d5ba9cd56a1a278f1
Niscolinx <munisco12@gmail.com>
2020-05-24 21:25:21 +0100

    Building the services area to load in a box

--- src/components/Layout/Hero.js
+++ src/components/Layout/Hero.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
 import { Container, Row, Col } from 'react-bootstrap'
 import imgUrl from '../../assets/images/bg_home.jpg'
 
