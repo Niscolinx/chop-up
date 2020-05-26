3b80d89eaecc99ad166a85ca0c08c3c3f7c2d910
Niscolinx <munisco12@gmail.com>
2020-05-26 12:49:22 +0100

    Adding the modal

--- src/components/Modal/SelectOptions.js
+++ src/components/Modal/SelectOptions.js
@@ -8,38 +8,37 @@ const SelectOptions = (props) => {
     return (
 
         <Aux>
-            <div className='services'>
 
-                <Row>
+            <Row>
 
-                    <h5 className='services_heading'>Select Service</h5>
-                </Row>
-                <Row className='services-box'>
+                <h5 className='services_heading'>Select Service</h5>
+            </Row>
+            <Row className='services-box'>
 
-                    <Col>
-                        <h5 className='services-box_title'>Restaurants</h5>
-                        <p className='services-box_description'>Food you love, from restaurants near you</p>
-                    </Col>
-                    <Col>
-                        <h5 className='services-box_title'>SuperMarkets</h5>
-                        <p className='services-box_description'>Food you love, from restaurants near you</p>
-                    </Col>
+                <Col>
+                    <h5 className='services-box_title'>Restaurants</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
+                <Col>
+                    <h5 className='services-box_title'>SuperMarkets</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
 
-                </Row>
-                <Row className='services-box'>
+            </Row>
+            <Row className='services-box'>
 
-                    <Col>
-                        <h5 className='services-box_title'>Pharmacies</h5>
-                        <p className='services-box_description'>Food you love, from restaurants near you</p>
-                    </Col>
-                    <Col>
-                        <h5 className='services-box_title'>Shopping</h5>
-                        <p className='services-box_description'>Food you love, from restaurants near you</p>
-                    </Col>
+                <Col>
+                    <h5 className='services-box_title'>Pharmacies</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
+                <Col>
+                    <h5 className='services-box_title'>Shopping</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
 
-                </Row>
+            </Row>
 
-            </div>
+
         </Aux>
     )
 }
