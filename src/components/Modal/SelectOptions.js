b65863eb1c8911cf0ba4bc979983137f995e00f9
Niscolinx <munisco12@gmail.com>
2020-05-26 12:47:34 +0100

    Adding the modal

--- src/components/Modal/SelectOptions.js
+++ src/components/Modal/SelectOptions.js
@@ -5,7 +5,36 @@ const SelectOptions = (props) => {
 
     return(
         <Aux>
-            
+            <Row>
+
+                <h5 className='services_heading'>Select Service</h5>
+            </Row>
+            <Row className='services-box'>
+
+                <Col>
+                    <h5 className='services-box_title'>Restaurants</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
+                <Col>
+                    <h5 className='services-box_title'>SuperMarkets</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
+
+            </Row>
+            <Row className='services-box'>
+
+                <Col>
+                    <h5 className='services-box_title'>Pharmacies</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
+                <Col>
+                    <h5 className='services-box_title'>Shopping</h5>
+                    <p className='services-box_description'>Food you love, from restaurants near you</p>
+                </Col>
+
+            </Row>
+
+
         </Aux>
     )
 }
