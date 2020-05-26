18ab783f514424ac101fd54e43e3f90e6a0e0f8d
Niscolinx <munisco12@gmail.com>
2020-05-26 13:20:03 +0100

    Adding the modal

--- src/components/Layout/Hero.js
+++ src/components/Layout/Hero.js
@@ -84,8 +84,8 @@ class Hero extends Component {
     };
     handleAreaChange = (inputValue: any, actionMeta: any) => {
 
-        console.group('Input Changed', inputValue, 'the Changed value is', actionMeta);
-        console.groupEnd();
+        // console.group('Input Changed', inputValue, 'the Changed value is', actionMeta);
+        // console.groupEnd();
 
         if (actionMeta.action === 'set-value') {
             this.setState({
