a2c412892409418d44cede62769f948d6cad4eb4
Niscolinx <munisco12@gmail.com>
2020-05-26 13:38:49 +0100

    Adding the modal

--- src/components/Modal/Backdrop.js
+++ src/components/Modal/Backdrop.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+console.log('this is the backdrop')
 const backdrop = (props) => (
     props.show? <div className = 'backdrop' onClick = {props.clicked}></div>: null
 )
