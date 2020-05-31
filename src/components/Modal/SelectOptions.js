f9a56210d64f3a49ce231bee05d3f6c2a80cb95d
Niscolinx <munisco12@gmail.com>
2020-05-31 22:01:29 +0100

    building the options to select from

--- src/components/Modal/SelectOptions.js
+++ src/components/Modal/SelectOptions.js
@@ -6,13 +6,34 @@ import { Row, Col } from 'react-bootstrap'
 const SelectOptions = (props) => {
 
     const options = {
+        Restaurants: {
+            Absu: ['AbsuRes1', 'AbsuRes2'],
+            Uturu: ['UturuRes1', 'UturuRes2'],
+            Okigwe: ['OkigweRes1', 'OkigweRes2'],
+            Isuochi: ['IsuochiRes1', 'IsuochiRes2'],
+            Isukwuato: ['IsukwuatoRes1', 'IsukwuatoRes2']
+        },
+        SuperMarkets: {
+            Absu: ['AbsuMarket1', 'AbsuMarket2'],
+            Uturu: ['UturuMarket1', 'UturuMarket2'],
+            Okigwe: ['OkigweMarket1', 'OkigweMarket2'],
+            Isuochi: ['IsuochiMarket1', 'IsuochiMarket2'],
+            Isukwuato: ['IsukwuatoMarket1', 'IsukwuatoMarket2']
+        },
+        Pharmacies: {
+            Absu: ['AbsuRes1', 'AbsuRes2'],
+            Uturu: ['UturuRes1', 'UturuRes2'],
+            Okigwe: ['OkigweRes1', 'OkigweRes2'],
+            Isuochi: ['IsuochiRes1', 'IsuochiRes2'],
+            Isukwuato: ['IsukwuatoRes1', 'IsukwuatoRes2']
+        },
         restaurants: {
             Absu: ['AbsuRes1', 'AbsuRes2'],
             Uturu: ['UturuRes1', 'UturuRes2'],
             Okigwe: ['OkigweRes1', 'OkigweRes2'],
-            Isuochi: [],
-            Isukwuato: []
-        }
+            Isuochi: ['IsuochiRes1', 'IsuochiRes2'],
+            Isukwuato: ['IsukwuatoRes1', 'IsukwuatoRes2']
+        },
     }
 
     return (
