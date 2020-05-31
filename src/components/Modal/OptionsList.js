3ae241a942c7b190ce94e55996c6758783142270
Niscolinx <munisco12@gmail.com>
2020-05-31 23:03:09 +0100

    building the options to select from

--- src/components/Modal/OptionsList.js
+++ src/components/Modal/OptionsList.js
@@ -48,6 +48,6 @@ export const OptionsMap = (props) => {
     return <Col className='services-box_item' onClick={OptionsMap}>
         <h5 className='services-box_title'>{props.title}</h5>
         <p className='services-box_description'>{props.description}</p>
-        <p className='services-box_description'>{this.mapSelections}</p>
+        <p className='services-box_description'>{this.mapSelections()}</p>
     </Col>
 }
