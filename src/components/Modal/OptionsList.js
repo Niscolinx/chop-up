752d67d790aa2c4e0a373d7ad5d7cafa0f07eded
Niscolinx <munisco12@gmail.com>
2020-06-01 06:46:52 +0100

    building the options to select from

--- src/components/Modal/OptionsList.js
+++ src/components/Modal/OptionsList.js
@@ -36,8 +36,7 @@ const options = {
 let listCategory = []
 const mapSelections = (props) => {
     const optionsMap = Object.keys(options[props].Absu).map(singleOption => {
-           let compileList = listCategory.push(singleOption)
-           return compileList
+        return listCategory.push(Object.keys(options[props].Absu[singleOption])
         })
 
 
