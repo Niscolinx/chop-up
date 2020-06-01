b1bf8f956eaf7695b8704b1830b8710d6ed99b54
Niscolinx <munisco12@gmail.com>
2020-06-01 06:54:32 +0100

    building the options to select from

--- src/components/Modal/OptionsList.js
+++ src/components/Modal/OptionsList.js
@@ -38,13 +38,13 @@ const mapSelections = (props) => {
     const optionsMap = Object.keys(options[props].Absu).map(singleOption => {
         console.log('inner loop', options[props].Absu[singleOption])
         return options[props].Absu[singleOption]
-        
-        })
 
-    console.log('The list',optionsMap)
+    })
+
+    console.log('The list', optionsMap)
 }
 export const OptionsMap = (props) => {
-console.log('the category', listCategory)
+    console.log('the category', listCategory)
     return <Col className='services-box_item' onClick={OptionsMap}>
         <h5 className='services-box_title'>{props.title}</h5>
         <p className='services-box_description'>{props.description}</p>
