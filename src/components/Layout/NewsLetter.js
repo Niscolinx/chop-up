33b4a82130b1948351fce0ec46c3c39051e29dd5
Niscolinx <munisco12@gmail.com>
2020-06-03 10:44:12 +0100

    transformed the newsLetter into a class component

--- src/components/Layout/NewsLetter.js
+++ src/components/Layout/NewsLetter.js
@@ -1,56 +1,102 @@
-import React from 'react'
-import { Container, Row, Col, Button, Card, CardGroup } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 
-const NewsLetter = () => {
-    return(
-        <div className='newsletter'>
+class NewsLetter extends Component {
 
-            <Container>
-                <div className='newsletter_heading'>
-                    <h1>20% Off on your first order</h1>
-                    <h1>by subscribing to our newsletter!</h1>
-                </div>
-                <Row >
-                    <Col>
-                        <p className="newsletter_description">Don't miss out on your great offers and Receive deals from out top vendors via e-mail</p>
-                    </Col>
-                </Row>
+    state = {
+        control: {
+            email: {
+                elementType: 'input',
+                config: {
+                    type: 'text',
+                    placeholder: 'Email'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                isTouched: false
+            },
+            password: {
+                elementType: 'input',
+                config: {
+                    type: 'password',
+                    placeholder: 'password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                isTouched: false
+            }
+        },
+    }
 
-                <Form>
-                    <Form.Row className='newsletter_form'>
+    render() {
 
-                        <Form.Group as={Col} controlId="formGridPassword" className='newsletter_form-item'>
-                            <Form.Control as="select">
-                                <option>Absu</option>
-                                <option>2</option>
-                                <option>3</option>
+        const formBtn = (e) => {
+            e.preventDefault()
 
-                            </Form.Control>
-                        </Form.Group>
-                      
+        }
+        return (
+            <div className='newsletter'>
 
-                        <Form.Group as={Col} controlId="formGridEmail" className='newsletter_form-item'>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
-                            
-                        <Form.Group as={Col} controlId="formGridEmail" className='newsletter_form-item'>
-                            <Button type="submit" className='newsletter_form-submitBtn'>
+                <Container>
+                    <div className='newsletter_heading'>
+                        <h1>20% Off on your first order</h1>
+                        <h1>by subscribing to our newsletter!</h1>
+                    </div>
+                    <Row >
+                        <Col>
+                            <p className="newsletter_description">Don't miss out on your great offers and Receive deals from out top vendors via e-mail</p>
+                        </Col>
+                    </Row>
+
+                    <Form>
+
+
+                        <Form.Row className='newsletter_form'>
+
+                            <Form.Group as={Col} controlId="formGridPassword" className='newsletter_form-item'>
+                                <Form.Control as="select">
+                                    <option>Absu</option>
+                                    <option>2</option>
+                                    <option>3</option>
+
+                                </Form.Control>
+                            </Form.Group>
+
+
+                            <Form.Group as={Col} controlId="formGridEmail" className='newsletter_form-item'>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridEmail" className='newsletter_form-item'>
+                                <Button type="submit" className='newsletter_form-submitBtn' onClick={formBtn}>
+                                    subscribe to Newsletter
+                                </Button>
+                            </Form.Group>
+
+                            <Button type="submit" className='newsletter_form-submitBtn' onClick={formBtn}>
                                 subscribe to Newsletter
                                 </Button>
+
+
+
+
+                        </Form.Row>
+                        <Form.Group controlId="formBasicCheckbox" className='newsletter_description'>
+                            <Form.Check type="checkbox" label="I have read and accepted the terms and conditions and privacy policy" />
                         </Form.Group>
-                     
-                        
-
-
-                    </Form.Row>
-                    <Form.Group controlId="formBasicCheckbox" className='newsletter_description'>
-                        <Form.Check type="checkbox" label="I have read and accepted the terms and conditions and privacy policy" />
-                    </Form.Group>
-                </Form>
-            </Container>
-        </div>
-    )
+                    </Form>
+                </Container>
+            </div>
+        )
+    }
 }
 
 export default NewsLetter;
