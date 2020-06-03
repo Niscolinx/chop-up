af527c31419b1acd7e234afc4785c07938570f53
Niscolinx <munisco12@gmail.com>
2020-06-03 11:03:58 +0100

    transformed the newsLetter into a class component

--- src/components/Layout/NewsLetter.js
+++ src/components/Layout/NewsLetter.js
@@ -55,7 +55,7 @@ class NewsLetter extends Component {
             })
         }
 
-        let form = <div className='contactForm'>
+        let form = <div className='newsLetterForm'>
             {elementTypeArr.map(elementType => {
                 return <Input
                     isTouched={elementType.config.isTouched}
