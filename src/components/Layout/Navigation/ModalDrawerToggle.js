e065a394381851580bedd5f8e4aa2c9de22682c2
Niscolinx <munisco12@gmail.com>
2020-06-09 10:28:35 +0100

    styling the modal toggle

--- src/components/Layout/Navigation/ModalDrawerToggle.js
+++ src/components/Layout/Navigation/ModalDrawerToggle.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const ModalToggle = (props) => (
     <div onClick={props.clicked} className='modalToggle'>
+        <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+
         &times;
     </div>
 )
