77a4c20ae74d36324e160a615ca6c7881e4ebea0
Niscolinx <munisco12@gmail.com>
2020-06-09 21:10:16 +0100

    Added icons in the simple steps

---
+++ src/containers/Vendors.js
@@ -0,0 +1,16 @@
+import React, { Component  } from "react"
+
+
+class Vendors extends Component{
+    render(){
+        return(
+
+            <div>
+                The Vendors page
+            </div>
+        )
+    }
+}
+
+
+export default Vendors
